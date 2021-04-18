4ab5aa377e4e742cde583bff4d9a71951d64c5f1
Yangtao(Max) Zhang <maxzhang@umich.edu>
2021-04-18 17:27:42 -0400

    finish linking

--- pages/index.js
+++ pages/index.js
@@ -1,84 +1,114 @@
 import { Form, Select, InputNumber, Switch, Slider, Button} from 'antd'
 import { Row, Col, Divider, Checkbox } from 'antd';
 import { Radio, Space } from 'antd';
-
-// Custom DatePicker that uses Day.js instead of Moment.js
-import DatePicker from '../components/DatePicker'
-
-import { SmileFilled } from '@ant-design/icons'
-
+import React, {Fragment, useState, useEffect, Component } from 'react';
 import Link from 'next/link'
-
-const FormItem = Form.Item
-const Option = Select.Option
 
 const content = {
   marginTop: '100px',
 }
 
-export default function Home() {
-  return (
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        crypo: "ETH-BTC",
+        markets: ["0"],
+        timeframe: "1m"
+    }
+  }
+  onChangeCrypo = (val) => {
+    console.log(Object.keys(val))
+    console.log(val)
+    this.setState({
+      "crypo": val.target.value,
+    })
+  }
+
+  onChangeMarkets = (val) => {
+    this.setState({
+      "markets": val.sort(),
+    })
+  }
+
+  onChangeTimeframe = (val) => {
+    this.setState({
+      "timeframe": val.target.value
+    })
+  }
+
+  render (){
+    const {crypo, markets, timeframe} = this.state;
+    let url = "/stock?"
+    let searchParams = new URLSearchParams();
+    searchParams.append("pair", crypo)
+    searchParams.append("market", markets.join("-"))
+    searchParams.append("timeframe", timeframe)
+    url += searchParams.toString()
+    console.log(url)
+  return(
     <div style={content}>
-      <Row justify="space-around" style={{fontSize:38, fontWeight:'bold', color:'Black', marginBottom: 24}}>View &amp; Compare Crypto Info</Row>
-      <Divider style={{fontSize:24, fontWeight:'bold', color:'Black'}} plain>Crypto</Divider>
-      <Row justify="space-around" style={{fontSize:16, marginTop: -15, marginBottom: 24}}>Please choose the crypto you want to view.</Row>
-      <Row justify="space-around">
-        <Radio.Group defaultValue="a" buttonStyle="solid">
-        <Row style={{marginBottom:24}}>
-          <Radio.Button value="a" style={{width: 300, height: 40, fontSize: 20, marginLeft: 80, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>BTC / USDT</Radio.Button>
-          <Radio.Button value="b" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>BNB / BUSD</Radio.Button>
-          <Radio.Button value="c" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>ETH / BUSD</Radio.Button>
-          <Radio.Button value="d" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 80, textAlign:'center', fontWeight:'bold'}}>ZRP / Ripple</Radio.Button>
-        </Row>
-        <Row style={{marginBottom:24}}>
-          <Radio.Button value="e" style={{width: 300, height: 40, fontSize: 20, marginLeft: 80, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>BTC / USDT</Radio.Button>
-          <Radio.Button value="f" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>BNB / BUSD</Radio.Button>
-          <Radio.Button value="g" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>ETH / BUSD</Radio.Button>
-          <Radio.Button value="h" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 80, textAlign:'center', fontWeight:'bold'}}>ZRP / Ripple</Radio.Button>
-        </Row>
-        </Radio.Group>
+    <Row justify="space-around" style={{fontSize:38, fontWeight:'bold', color:'Black', marginBottom: 24}}>View &amp; Compare Crypto Info</Row>
+    <Divider style={{fontSize:24, fontWeight:'bold', color:'Black'}} plain>Crypto</Divider>
+    <Row justify="space-around" style={{fontSize:16, marginTop: -15, marginBottom: 24}}>Please choose the crypto you want to view.</Row>
+    <Row justify="space-around">
+      <Radio.Group defaultValue="ETH-BTC" buttonStyle="solid" onChange={this.onChangeCrypo}>
+      <Row style={{marginBottom:24}}>
+        <Radio.Button value="ETH-BTC" style={{width: 300, height: 40, fontSize: 20, marginLeft: 80, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>ETH/BTC</Radio.Button>
+        <Radio.Button value="ZEC-BTC" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>ZEC/BTC</Radio.Button>
+        <Radio.Button value="LTC-BTC" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>LTC/BTC</Radio.Button>
+        <Radio.Button value="LINK-BTC" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 80, textAlign:'center', fontWeight:'bold'}}>LINK/BTC</Radio.Button>
       </Row>
+      <Row style={{marginBottom:24}}>
+        <Radio.Button value="ALGO-BTC" style={{width: 300, height: 40, fontSize: 20, marginLeft: 80, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>ALGO/BTC</Radio.Button>
+        <Radio.Button value="OMG-BTC" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>OMG/BTC</Radio.Button>
+        <Radio.Button value="ATOM-BTC" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>ATOM/BTC</Radio.Button>
+        <Radio.Button value="DASH-BTC" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 80, textAlign:'center', fontWeight:'bold'}}>DASH/BTC</Radio.Button>
+      </Row>
+      </Radio.Group>
+    </Row>
 
-      <Divider style={{fontSize:24, fontWeight:'bold', color:'Black'}} plain>Market</Divider>
-      <Row justify="space-around" style={{fontSize:16, marginTop: -15, marginBottom: 24}}>Please choose markets you want to compare.</Row>
-      <Row justify="space-around">
-        <Checkbox.Group onChange={onChange}>
-          <Row style={{marginBottom:24}}>
-            <Col><Checkbox value="A" style={{width: 300, height: 40, marginLeft: 80, marginRight: 15, fontSize: 20, fontWeight:'bold'}}>Market 1</Checkbox></Col>
-            <Col><Checkbox value="B" style={{width: 300, height: 40, marginLeft: 15, marginRight: 15, fontSize: 20, fontWeight:'bold'}}>Market 2</Checkbox></Col>
-            <Col><Checkbox value="C" style={{width: 300, height: 40, marginLeft: 15, marginRight: 15, fontSize: 20, fontWeight:'bold'}}>Market 3</Checkbox></Col>
-            <Col><Checkbox value="D" style={{width: 300, height: 40, marginLeft: 15, marginRight: 80, fontSize: 20, fontWeight:'bold'}}>Market 4</Checkbox></Col>
-          </Row>
-        </Checkbox.Group>
-      </Row>
-
-      <Divider style={{fontSize:24, fontWeight:'bold', color:'Black'}} plain>Information</Divider>
-      <Row justify="space-around" style={{fontSize:16, marginTop: -15, marginBottom: 24}}>Please choose how you want to view the information.</Row>
-      <Row justify="space-around">
-        <Radio.Group defaultValue="a" buttonStyle="solid">
+    <Divider style={{fontSize:24, fontWeight:'bold', color:'Black'}} plain>Market</Divider>
+    <Row justify="space-around" style={{fontSize:16, marginTop: -15, marginBottom: 24}}>Please choose markets you want to compare.</Row>
+    <Row justify="space-around">
+      <Checkbox.Group onChange={this.onChangeMarkets}>
         <Row style={{marginBottom:24}}>
-          <Col><Radio.Button value="a" style={{width: 300, height: 40, fontSize: 20, marginLeft: 80, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>1 min</Radio.Button></Col>
-          <Col><Radio.Button value="b" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>15 min</Radio.Button></Col>
-          <Col><Radio.Button value="c" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>1 hour</Radio.Button></Col>
-          <Col><Radio.Button value="d" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 80, textAlign:'center', fontWeight:'bold'}}>4 hour</Radio.Button></Col>
+          <Col><Checkbox value="0" style={{width: 300, height: 40, marginLeft: 80, marginRight: 15, fontSize: 20, fontWeight:'bold'}}>Binance</Checkbox></Col>
+          <Col><Checkbox value="1" style={{width: 300, height: 40, marginLeft: 15, marginRight: 15, fontSize: 20, fontWeight:'bold'}}>Huobi Pro</Checkbox></Col>
+          <Col><Checkbox value="2" style={{width: 300, height: 40, marginLeft: 15, marginRight: 15, fontSize: 20, fontWeight:'bold'}}>Coinbase Pro</Checkbox></Col>
+          <Col><Checkbox value="3" style={{width: 300, height: 40, marginLeft: 15, marginRight: 80, fontSize: 20, fontWeight:'bold'}}>Kraken</Checkbox></Col>
         </Row>
-        <Row style={{marginBottom:24}}>  
-          <Col><Radio.Button value="e" style={{width: 300, height: 40, fontSize: 20, marginLeft: 80, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>1 day</Radio.Button></Col>
-          <Col><Radio.Button value="f" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 80, textAlign:'center', fontWeight:'bold'}}>1 week</Radio.Button></Col>
-        </Row>
-        </Radio.Group>
-      </Row>
+      </Checkbox.Group>
+    </Row>
 
-      <Divider plain></Divider>
-      <Row justify="space-around" style={{marginBottom:24}}>
-      <Button type="primary" style={{width: 300, height: 40, fontSize: 20, textAlign:'center', fontWeight:'bold'}}>
-        View
-      </Button>
+    <Divider style={{fontSize:24, fontWeight:'bold', color:'Black'}} plain>Information</Divider>
+    <Row justify="space-around" style={{fontSize:16, marginTop: -15, marginBottom: 24}}>Please choose how you want to view the information.</Row>
+    <Row justify="space-around">
+      <Radio.Group defaultValue="1m" buttonStyle="solid" onChange={this.onChangeTimeframe}>
+      <Row style={{marginBottom:24}}>
+        <Col><Radio.Button value="1m" style={{width: 300, height: 40, fontSize: 20, marginLeft: 80, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>1 min</Radio.Button></Col>
+        <Col><Radio.Button value="5m" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>5 min</Radio.Button></Col>
+        <Col><Radio.Button value="15m" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>15 min</Radio.Button></Col>
+        <Col><Radio.Button value="1h" style={{width: 300, height: 40, fontSize: 20, marginLeft: 15, marginRight: 80, textAlign:'center', fontWeight:'bold'}}>1 hour</Radio.Button></Col>
       </Row>
-    </div>
+      <Row style={{marginBottom:24}}>  
+        <Col><Radio.Button value="1d" style={{width: 300, height: 40, fontSize: 20, marginLeft: 80, marginRight: 15, textAlign:'center', fontWeight:'bold'}}>1 day</Radio.Button></Col>
+      </Row>
+      </Radio.Group>
+    </Row>
+
+    <Divider plain></Divider>
+    <Row justify="space-around" style={{marginBottom:24}}>
+      <Link href={url}>
+    <Button type="primary" style={{width: 300, height: 40, fontSize: 20, textAlign:'center', fontWeight:'bold'}}>
+      View
+    </Button>
+      </Link>
+    </Row>
+  </div>
   )
+  }
 }
 
-function onChange(checkedValues) {
-  console.log('checked = ', checkedValues);
-}
+export default Home
--- pages/stock.js
+++ pages/stock.js
@@ -1,33 +1,147 @@
-import React, { useState, useEffect } from 'react';
-import dynamic from 'next/dynamic'
-// import StockChart from '../components/StockChart'
-// import {Stock} from '@ant-design/charts'
+import React, {Fragment, useState, useEffect } from 'react';
+import { Row, Col, Card, Tag, Statistic, Form, Select, InputNumber, Switch, Slider, Button } from 'antd'
+import { ArrowUpOutlined, ArrowDownOutlined, MoneyCollectOutlined } from '@ant-design/icons';
 import Stock from '@ant-design/charts/lib/stock';
+import { useRouter } from 'next/router'
 const ccxt = require ('ccxt');
-// const StockChartWithNoSSR = dynamic(
-//   import('../components/StockChart'),
-//   { ssr: false }
-// )
 
 const StockVis = (data) => {
-    console.log(data)
-    var config = {
-        width: 400,
-        height: 500,
-        data: data.data,
-        xField: 'trade_date',
-        yField: ['open', 'close', 'high', 'low'],
-      };
-    return <Stock {...config} />;
+    const router = useRouter()
+    console.log(router.query)
+    let pair = router.query.pair;
+    let markets = router.query.market
+    console.log(pair, markets)
+    pair = pair.replace("-", "/")
+    let marketsArray = markets.split("-")
+    let marketCount = marketsArray.length;
+    console.log(marketCount)
+    console.log(data.stockData.length)
+    const gridSize = 24 / marketCount;
+    return (
+        <Fragment>
+        <Row>
+            <Col span={8}>
+            <Card>
+                <Statistic
+                    title="Max Value to Sell"
+                    value={data.maxSell.val}
+                    precision={6}
+                    valueStyle={{ color: '#cf1322' }}
+                    prefix={<ArrowUpOutlined />}
+                />
+                <Tag color="error">{data.maxSell.marketName}</Tag>
+                </Card>
+            </Col>
+            <Col span={8}>
+            <Card>
+                <Statistic
+                    title="Min Value to Buy"
+                    value={data.minBuy.val}
+                    precision={6}
+                    valueStyle={{ color:  '#3f8600'}}
+                    prefix={<ArrowDownOutlined />}
+                />
+                <Tag color="success">{data.minBuy.marketName}</Tag>
+                </Card>
+            </Col>
+            <Col span={8}>
+            <Card>
+                <Statistic
+                    title="Profit"
+                    value={data.profit}
+                    precision={2}
+                    valueStyle={{ color: '#FFD700' }}
+                    prefix={<MoneyCollectOutlined />}
+                    suffix="%"
+                />
+                </Card>
+            </Col>
+        </Row>
+        <Row>
+            {
+                data.stockData.map(marketData => (
+                    <Col span={gridSize}>
+                        <Card title={marketData.name} bordered={false}>
+                    <Stock data={marketData.data} xField={'trade_date'} yField={['open', 'close', 'high', 'low']} />
+                    </Card>
+                    </Col>
+                ))
+            }
+        </Row>
+        </Fragment>
+    );
 }
 
-export async function getStaticProps(){
-    const exchange = new ccxt.kraken ()
-    const ohlcv = await exchange.fetchOHLCV ('BTC/USD', '1h')
-    const series = ohlcv.slice(-24).map (x => ({"trade_date":new Date(x[0]).toISOString(),"close":x[3], "open":x[1], "high":x[2], "low":x[3], }))
+export async function getServerSideProps(context){
+    // const router = useRouter()
+    // let {pair, markets, timeframe} = context.query
+    const namesAll = ["Binance", "Huobi Pro", "Coinbase Pro", "Kraken"];
+    let pair = context.query.pair;
+    let markets = context.query.market
+    let timeframe = context.query.timeframe
+    pair = pair.replace("-", "/")
+    console.log(pair, markets, timeframe)
+    let marketsArray = markets.split("-")
+    let namesCurrent = marketsArray.map(item => namesAll[parseInt(item)])
+    let stockData = [];
+    let minPrice = [];
+    let maxPrice = [];
+    for (let market of marketsArray){
+        let exchange;
+        let name;
+        switch(parseInt(market)){
+            case 0:
+                exchange = new ccxt.binance()
+                name = "Binance"
+                break
+            case 1:
+                exchange = new ccxt.huobipro()
+                name = "Huobi Pro"
+                break
+            case 2:
+                exchange = new ccxt.coinbasepro()
+                name = "Coinbase Pro"
+                break
+            case 3:
+                exchange = new ccxt.kraken()
+                name = "Kraken"
+                break
+        }
+        let ohlcv = await exchange.fetchOHLCV (pair, timeframe)
+        let series;
+        switch(timeframe){
+            case "1m":
+                series = ohlcv.slice(-60)
+                break
+            case "5m":
+                series = ohlcv.slice(-72)
+                break
+            case "1h":
+                series = ohlcv.slice(-24)
+                break
+            case "1d":
+                series = ohlcv.slice(-30)
+                break
+            case "1w":
+                series = ohlcv.slice(-12)
+                break
+            case "15m":
+                series = ohlcv.slice(-36)
+                break
+        }
+        let data = series.map (x => ({"trade_date":new Date(x[0]).toISOString(),"close":x[3], "open":x[1], "high":x[2], "low":x[3], }))
+        stockData.push({"data":data, "name":name})
+        minPrice.push(Math.min(...data.map(item => item.low)))
+        maxPrice.push(Math.max(...data.map(item => item.high)))
+    }
+    const maxVal = Math.max(...maxPrice)
+    const minVal = Math.min(...minPrice)
     return {
         props:{
-            data: series
+            stockData: stockData,
+            maxSell: {marketName: namesCurrent[maxPrice.indexOf(maxVal)], val: maxVal},
+            minBuy: {marketName: namesCurrent[minPrice.indexOf(minVal)], val: minVal},
+            profit: (maxVal - minVal) / minVal * 100
         }
     }
 }
